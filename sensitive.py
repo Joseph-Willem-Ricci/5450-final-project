58bf5720b6aa4cd4f842b6aeb8e283e31e6278d5
Joseph Willem Ricci <josephwillemricci@gmail.com>
2023-04-28 15:54:21 -0400

    Update sensitive.py

--- sensitive.py
+++ sensitive.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+# some multiples due to the possibility of similar looking but different characters
 sensitive_words = ['bitch', 'bitches', 'nigga', 'niggas', 'pussy', 'slut', 'hoe', 'bitches', 'pussys', 'fuck', 'ass', 
-                   'damn', 'dick', 'fucking', 'fuckin', 'motherfuckin', 'fucking', 'fucking']
+                   'damn', 'dick', 'fucking', 'fuckin', 'motherfuckin', 'fucking', 'fucking', 'bitch', 'bitch']
 
 words_ser = pd.Series(sensitive_words)
 
